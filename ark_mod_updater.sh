bb0857979bb541110a35a7e49b21bb67fdfd06f0
Lacrimosa99 <lacrimosa99@gmx.de>
2017-05-08 19:45:08 +0200

    EMail System reworked
    the complete email system is reworked for better checks, if a mail on
    same day send

--- ark_mod_updater.sh
+++ ark_mod_updater.sh
@@ -16,7 +16,7 @@ SUBJECT="ARK Mod-ID failure detected on $(hostname)"
 ######## from here nothing change ########
 ##########################################
 
-CURRENT_UPDATER_VERSION="1.6"
+CURRENT_UPDATER_VERSION="1.7"
 ARK_APP_ID="346110"
 MASTER_PATH="/home/$MASTERSERVER_USER"
 STEAM_CMD_PATH="$MASTER_PATH/masterserver/steamCMD/steamcmd.sh"
@@ -24,12 +24,15 @@ ARK_MOD_PATH="$MASTER_PATH/masteraddons"
 LOG_PATH="$MASTER_PATH/logs"
 MOD_LOG="$LOG_PATH/ark_mod_id.log"
 MOD_BACKUP_LOG="$LOG_PATH/ark_mod_id_backup.log"
-INSTALL_LOG=""$LOG_PATH"/ark_mod_update_status_$(date +"%d-%m-%Y").log"
-DEPRECATED_LOG=""$LOG_PATH"/ark_mod_deprecated_$(date +"%d-%m-%Y").log"
+INSTALL_LOG="$LOG_PATH/ark_mod_update_status_$(date +"%d-%m-%Y").log"
 MOD_NO_UPDATE_LOG="$LOG_PATH/ark_mod_id_no_update.log"
 MOD_LAST_VERSION="$MASTER_PATH/versions"
 TMP_PATH="$MASTER_PATH/temp"
-EMAIL_MESSAGE=""$TMP_PATH"/emailmessage.txt"
+EMAIL_TMP_MESSAGE="$TMP/email.txt"
+EMAIL_SCRIPT_FAILURE_LOG="$LOG_PATH/ark_mod_failure_email_$(date +"%d-%m-%Y").txt"
+EMAIL_SCRIPT_OUTDATED_LOG="$LOG_PATH/ark_mod_outdated_email_$(date +"%d-%m-%Y").txt"
+EMAIL_MOD_DEPRECATED_LOG="$LOG_PATH/ark_mod_deprecated_email_$(date +"%d-%m-%Y").txt"
+EMAIL_MOD_DL_FAILURE_LOG="$LOG_PATH/ark_mod_dl_failure_email_$(date +"%d-%m-%Y").txt"
 DEAD_MOD="deprec|outdated|brocken|not-supported|mod-is-dead|no-longer-|old|discontinued"
 
 PRE_CHECK() {
@@ -39,31 +42,32 @@ PRE_CHECK() {
 
 		USER_CHECK=$(cut -d: -f6,7 /etc/passwd | grep "$MASTERSERVER_USER" | head -n1)
 		if ([ ! "$USER_CHECK" == "/home/$MASTERSERVER_USER:/bin/bash" -a ! "$USER_CHECK" == "/home/$MASTERSERVER_USER/:/bin/bash" ]); then
-			FAILURE_MESSAGE="User $MASTERSERVER_USER not found or wrong shell rights!\nPlease check the Masteruser inside this Script or the user shell rights."
-			TMP_FAILURE_EMAIL
+			EMAIL_SCRIPT_FAILURE="1"
+			EMAIL_SCRIPT_FAILURE_MESSAGE="User $MASTERSERVER_USER not found or wrong shell rights!\nPlease check the Masteruser inside this Script or the user shell rights."
 		fi
 
 		if [ ! -d "$ARK_MOD_PATH" ]; then
-			FAILURE_MESSAGE='Masteraddons Directory "$ARK_MOD_PATH" not found!'
-			TMP_FAILURE_EMAIL
+			EMAIL_SCRIPT_FAILURE="1"
+			EMAIL_SCRIPT_FAILURE_MESSAGE="Masteraddons Directory $ARK_MOD_PATH not found!"
 		fi
 
 		if [ ! -f "$STEAM_CMD_PATH" ]; then
-			FAILURE_MESSAGE='Steam Installation "$STEAM_CMD_PATH" found!'
-			TMP_FAILURE_EMAIL
+			EMAIL_SCRIPT_FAILURE="1"
+			EMAIL_SCRIPT_FAILURE_MESSAGE="Steam Installation $STEAM_CMD_PATH found!"
 		fi
 	else
-		FAILURE_MESSAGE='Variable "MASTERSERVER_USER" are empty!'
-		TMP_FAILURE_EMAIL
+		EMAIL_SCRIPT_FAILURE="1"
+		EMAIL_SCRIPT_FAILURE_MESSAGE="Variable MASTERSERVER_USER are empty!"
 	fi
 	sleep 2
 
 	LATEST_UPDATER_VERSION=`wget -q --timeout=60 -O - https://api.github.com/repos/Lacrimosa99/Easy-WI-ARK-Mod-Updater/releases/latest | grep -Po '(?<="tag_name": ")([0-9]\.[0-9])'`
 	if [ "`printf "${LATEST_UPDATER_VERSION}\n${CURRENT_UPDATER_VERSION}" | sort -V | tail -n 1`" != "$CURRENT_UPDATER_VERSION" ]; then
+		EMAIL_SCRIPT_OUTDATED="1"
 		echo >> "$INSTALL_LOG"
-		echo "You are using a old ARK Mod Updater Script Version ${CURRENT_UPDATER_VERSION}."	| tee -a "$INSTALL_LOG" "$EMAIL_MESSAGE"
-		echo "Please upgrade to Version ${LATEST_UPDATER_VERSION} over the ark_mod_manager.sh Script and retry." | tee -a "$INSTALL_LOG" "$EMAIL_MESSAGE"
-		echo "Updater worked currently, but all script fixes and script updates are currently not available." | tee -a "$INSTALL_LOG" "$EMAIL_MESSAGE"
+		EMAIL_SCRIPT_OUTDATED_MESSAGE="You are using a old ARK Mod Updater Script Version ${CURRENT_UPDATER_VERSION}."	| tee -a "$INSTALL_LOG" "$EMAIL_TMP_MESSAGE"
+		EMAIL_SCRIPT_OUTDATED_MESSAGE="Please upgrade to Version ${LATEST_UPDATER_VERSION} over the ark_mod_manager.sh Script and retry." | tee -a "$INSTALL_LOG" "$EMAIL_TMP_MESSAGE"
+		EMAIL_SCRIPT_OUTDATED_MESSAGE="Updater worked currently, but all fixes and updates are currently not available." | tee -a "$INSTALL_LOG" "$EMAIL_TMP_MESSAGE"
 	fi
 
 	if [ ! -d "$MOD_LAST_VERSION" ]; then
@@ -76,7 +80,7 @@ PRE_CHECK() {
 	else
 		echo >> "$INSTALL_LOG"
 		echo "Updater is currently running... please try again later." >> "$INSTALL_LOG"
-		FINISHED_HEADER
+		FINISHED
 	fi
 }
 
@@ -93,7 +97,7 @@ UPDATE() {
 		exit 1
 	fi
 
-	CLEANFILES
+	rm -rf "$STEAM_WORKSHOP_PATH"
 	if [ -f "$MOD_LOG" ]; then
 		if [ -f "$MOD_BACKUP_LOG" ]; then
 			rm -rf "$MOD_BACKUP_LOG"
@@ -116,9 +120,9 @@ UPDATE() {
 		echo "Update canceled!" >> "$INSTALL_LOG"
 	fi
 
-	if [ -f "$TMP_PATH"/ark_update_failure.log ]; then
+	if [ -f "$TMP_PATH"/ark_update_failure.log]; then
 		SECOND_DOWNLOAD=1
-		CLEANFILES
+		rm -rf "$STEAM_WORKSHOP_PATH"
 		sleep 120
 		COUNTER=0
 		unset ARK_MOD_ID
@@ -186,7 +190,7 @@ INSTALL_CHECK() {
 						if [ "$COUNTER" = "3" ]; then
 							echo >> "$INSTALL_LOG"
 							if  [ "$SECOND_DOWNLOAD" = "1" ]; then
-							echo >> "$INSTALL_LOG"
+								echo >> "$INSTALL_LOG"
 								echo "Second Download:" >> "$INSTALL_LOG"
 								echo >> "$INSTALL_LOG"
 								echo >> "$INSTALL_LOG"
@@ -231,10 +235,14 @@ INSTALL_CHECK() {
 						echo "$ARK_LAST_CHANGES_DATE" > ""$MOD_LAST_VERSION"/ark_mod_id_"$MODID".txt"
 						chown -cR "$MASTERSERVER_USER":"$MASTERSERVER_USER" "$MOD_LAST_VERSION" 2>&1 >/dev/null
 					else
+						EMAIL_MOD_DEPRECATED="1"
 						if [ ! -f "$MOD_NO_UPDATE_LOG" ]; then
 							touch "$MOD_NO_UPDATE_LOG"
+            	MOD_NO_UPDATE_TMP_NAME=""
+						else
+            	MOD_NO_UPDATE_TMP_NAME=$(cat "$MOD_NO_UPDATE_LOG" | grep "$MODID")
 						fi
-						if [ $(MOD_TMP_NAME=$(cat "$MOD_NO_UPDATE_LOG" | grep "$MODID" )) = "" ]; then
+						if [ -f "$MOD_NO_UPDATE_LOG" -a "$MOD_NO_UPDATE_TMP_NAME" = "" ]; then
 							echo "$MODID" >> "$MOD_NO_UPDATE_LOG"
 						fi
 						sed -i "/$MODID/d" "$MOD_BACKUP_LOG"
@@ -359,34 +367,14 @@ DECOMPRESS() {
 }
 
 CLEANFILES() {
-	rm -rf "$STEAM_WORKSHOP_PATH"
-}
+	find "$LOG_PATH" -name "ark_mod_update_status_*" -mtime +7 -exec rm -rf {} \;
+	find "$LOG_PATH" -name "ark_mod_failure_email_*" -mtime +7 -exec rm -rf {} \;
+	find "$LOG_PATH" -name "ark_mod_outdated_email_*" -mtime +7 -exec rm -rf {} \;
+	find "$LOG_PATH" -name "ark_mod_deprecated_email_*" -mtime +7 -exec rm -rf {} \;
+	find "$LOG_PATH" -name "ark_mod_dl_failure_email_*" -mtime +7 -exec rm -rf {} \;
 
-FINISHED() {
 	if [ -f "$TMP_PATH"/ark_update_failure.log ]; then
-		echo | tee -a "$INSTALL_LOG" "$EMAIL_MESSAGE"
-		echo "Date: $(date +%d.%m.%Y_%H:%M)" >> "$EMAIL_MESSAGE"
-		echo "Hostname: $(hostname)" >> "$EMAIL_MESSAGE"
-		echo "IP: $(ip -4 -o addr show dev eth0 | awk '{split($4,a,"/") ;print a[1]}' | head -n1)"	>> "$EMAIL_MESSAGE"
-		echo >> "$EMAIL_MESSAGE"
-		echo "Error in Logfiles found!" | tee -a "$INSTALL_LOG" "$EMAIL_MESSAGE"
-		echo "Logfile Backup restored" >> "$EMAIL_MESSAGE"
-		echo >> "$EMAIL_MESSAGE"
-		echo "The following IDs were not downloaded:" | tee -a "$INSTALL_LOG" "$EMAIL_MESSAGE"
-		echo | tee -a "$INSTALL_LOG" "$EMAIL_MESSAGE"
-		cat "$TMP_PATH"/ark_update_failure.log | tee -a "$INSTALL_LOG" "$EMAIL_MESSAGE"
-		echo | tee -a "$INSTALL_LOG" "$EMAIL_MESSAGE"
 		cp "$MOD_BACKUP_LOG" "$MOD_LOG"
-	fi
-
-	if [ -f "$DEPRECATED_LOG" -a ! -f old_"$DEPRECATED_LOG" ]; then
-		echo "Date: $(date +%d.%m.%Y_%H:%M)" >> "$EMAIL_MESSAGE"
-		echo "Hostname: $(hostname)" >> "$EMAIL_MESSAGE"
-		echo "IP: $(ip -4 -o addr show dev eth0 | awk '{split($4,a,"/") ;print a[1]}' | head -n1)"	>> "$EMAIL_MESSAGE"
-		echo | tee -a "$INSTALL_LOG" "$EMAIL_MESSAGE"
-		echo "The following IDs are obsolete and have not been updated:" | tee -a "$INSTALL_LOG" "$EMAIL_MESSAGE"
-		cat "$DEPRECATED_LOG" | tee -a "$INSTALL_LOG" "$EMAIL_MESSAGE"
-		mv "$DEPRECATED_LOG" "$DEPRECATED_LOG"_old
 	fi
 
 	if [ -f "$TMP_PATH"/ark_custom_appid_tmp.log ]; then
@@ -397,53 +385,85 @@ FINISHED() {
 		rm -rf "$TMP_PATH"/ark_update_failure.log
 	fi
 
-	CLEANFILES
-
-	find "$LOG_PATH" -name "ark_mod_update_*" -mtime +5 -exec rm -rf {} \;
-	find "$LOG_PATH" -name "ark_mod_deprecated_*" -mtime +30 -exec rm -rf {} \;
 	chown -cR "$MASTERSERVER_USER":"$MASTERSERVER_USER" "$LOG_PATH"/* 2>&1 >/dev/null
 	chown -cR "$MASTERSERVER_USER":"$MASTERSERVER_USER" "$MOD_LAST_VERSION"/* 2>&1 >/dev/null
-
-	if [ -f "$TMP_PATH"/ark_mod_updater_status ]; then
-		rm -rf "$TMP_PATH"/ark_mod_updater_status
-	fi
-
-	FINISHED_HEADER
 }
 
-TMP_FAILURE_EMAIL() {
-	TMP_PATH="/tmp"
-	EMAIL_MESSAGE=""$TMP_PATH"/emailmessage.txt"
-	echo "Date: $(date +%d.%m.%Y_%H:%M)" >> "$EMAIL_MESSAGE"
-	echo "Hostname: $(hostname)" >> "$EMAIL_MESSAGE"
-	echo "IP: $(ip -4 -o addr show dev eth0 | awk '{split($4,a,"/") ;print a[1]}' | head -n1)"	>> "$EMAIL_MESSAGE"
-	echo >> "$EMAIL_MESSAGE"
-	echo "Failure Message:" >> "$EMAIL_MESSAGE"
-	echo -e "$FAILURE_MESSAGE" >> "$EMAIL_MESSAGE"
-	echo >> "$EMAIL_MESSAGE"
-	echo 'Ark Mod Updater has been stopped!' >> "$EMAIL_MESSAGE"
-	echo 'Please fix it in the next time.' >> "$EMAIL_MESSAGE"
-
-	FINISHED_HEADER
+SEND_EMAIL() {
+	mail -s "$SUBJECT" "$EMAIL_TO" < "$EMAIL_TMP_MESSAGE"
+	sleep 3
 }
 
-FINISHED_HEADER() {
-	if [ ! "$EMAIL_TO" = "" ] && [ -f "$EMAIL_MESSAGE" ] && [ ! -f "$EMAIL_MESSAGE".$(date +%d.%m.%Y) ]; then
-		mail -s "$SUBJECT" "$EMAIL_TO" < "$EMAIL_MESSAGE"
-		sleep 3
+FINISHED() {
+	if [ ! "$EMAIL_TO" = "" ]; then
+		if [ "$EMAIL_SCRIPT_FAILURE" = "1" -o "$EMAIL_SCRIPT_OUTDATED" = "1" -o "$EMAIL_MOD_DEPRECATED" = "1" -o -f "$TMP_PATH"/ark_update_failure.log ]; then
+			# EMAIL HEADER
+			echo >> "$EMAIL_TMP_MESSAGE"
+			echo "Date: $(date +%d.%m.%Y_%H:%M)" >> "$EMAIL_TMP_MESSAGE"
+			echo "Hostname: $(hostname)" >> "$EMAIL_TMP_MESSAGE"
+			echo "IP: $(ip -4 -o addr show dev eth0 | awk '{split($4,a,"/") ;print a[1]}' | head -n1)"	>> "$EMAIL_TMP_MESSAGE"
+			echo >> "$EMAIL_TMP_MESSAGE"
+			# Script Failure
+			if [ "$EMAIL_SCRIPT_FAILURE" = "1" ]; then
+				echo "Failure Message:" >> "$EMAIL_TMP_MESSAGE"
+				echo "$EMAIL_SCRIPT_FAILURE_MESSAGE" | tee -a "$INSTALL_LOG" "$EMAIL_TMP_MESSAGE"
+				echo >> "$EMAIL_TMP_MESSAGE"
+				echo 'Ark Mod Updater has been stopped!' >> "$EMAIL_TMP_MESSAGE"
+				echo 'Please fix it in the next time.' >> "$EMAIL_TMP_MESSAGE"
+			# Script Outdated
+			elif [ "$EMAIL_SCRIPT_OUTDATED" = "1" ]; then
+				echo "$EMAIL_SCRIPT_OUTDATED_MESSAGE" | tee -a "$INSTALL_LOG" "$EMAIL_TMP_MESSAGE"
+			# Mod Deprecated
+			elif [ "$EMAIL_MOD_DEPRECATED" = "1" ]; then
+				echo "The following IDs are obsolete and have not been updated:" >> "$EMAIL_TMP_MESSAGE"
+				cat "$DEPRECATED_LOG" >> "$EMAIL_TMP_MESSAGE"
+			# Download Failure
+			elif [ -f "$TMP_PATH"/ark_update_failure.log ]; then
+				echo "Error in Logfiles found!" | tee -a "$INSTALL_LOG" "$EMAIL_TMP_MESSAGE"
+				echo "Logfile Backup restored" | tee -a "$INSTALL_LOG" "$EMAIL_TMP_MESSAGE"
+				echo | tee -a "$INSTALL_LOG" "$EMAIL_TMP_MESSAGE"
+				echo "The following IDs were not downloaded:" | tee -a "$INSTALL_LOG" "$EMAIL_TMP_MESSAGE"
+				echo | tee -a "$INSTALL_LOG" "$EMAIL_TMP_MESSAGE"
+				cat "$TMP_PATH"/ark_update_failure.log | tee -a "$INSTALL_LOG" "$EMAIL_TMP_MESSAGE"
+				echo | tee -a "$INSTALL_LOG" "$EMAIL_TMP_MESSAGE"
+			fi
+		fi
 
-		if [ "$TMP_PATH" = "/tmp" ]; then
-			mv "$EMAIL_MESSAGE" "$EMAIL_MESSAGE".$(date +%d.%m.%Y) 2>&1 >/dev/null
-			find "$TMP_PATH" -name "emailmessage.txt.*" -mtime +3 -exec rm -rf {} \;
-		else
-			mv "$EMAIL_MESSAGE" "$EMAIL_MESSAGE".$(date +%d.%m.%Y) 2>&1 >/dev/null
-			find "$TMP_PATH" -name "emailmessage.txt.*" -mtime +3 -exec rm -rf {} \;
+		if [ -f "$EMAIL_TMP_MESSAGE" ] ; then
+			# Script Failure
+			if [ "$EMAIL_SCRIPT_FAILURE" = "1" -a ! -f "$EMAIL_SCRIPT_FAILURE_LOG" ]; then
+				SEND_EMAIL
+				mv "$EMAIL_TMP_MESSAGE" "$EMAIL_SCRIPT_FAILURE_LOG"
+			# Script Outdated
+			elif [ "$EMAIL_SCRIPT_OUTDATED" = "1" -a ! -f "$EMAIL_SCRIPT_OUTDATED_LOG" ]; then
+				SEND_EMAIL
+				mv "$EMAIL_TMP_MESSAGE" "$EMAIL_MOD_DEPRECATED_LOG"
+			# Mod Deprecated
+			elif [ "$EMAIL_MOD_DEPRECATED" = "1" ]; then
+				SEND_EMAIL
+				if [ -f "$EMAIL_MOD_DEPRECATED_LOG" ]; then
+					mv "$EMAIL_MOD_DEPRECATED_LOG" "$EMAIL_MOD_DEPRECATED_LOG"_old
+				fi
+				mv "$EMAIL_TMP_MESSAGE" "$EMAIL_MOD_DEPRECATED_LOG"
+			# Download Failure
+			elif [ -f "$TMP_PATH"/ark_update_failure.log ]
+				SEND_EMAIL
+			fi
+			find "$LOG_PATH" -name "ark_mod_emailmessage_*" -mtime +3 -exec rm -rf {} \;
 		fi
 	fi
+
+	CLEANFILES
+
+##########
 
 	echo "--------------------- Finished $(date +"%H:%M") ---------------------" >> "$INSTALL_LOG"
 	echo >> "$INSTALL_LOG"
 	echo >> "$INSTALL_LOG"
+
+	if [ -f "$TMP_PATH"/ark_mod_updater_status ]; then
+		rm -rf "$TMP_PATH"/ark_mod_updater_status
+	fi
 
 	if [ "$DEBUG" == "ON" ]; then
 		set +x
